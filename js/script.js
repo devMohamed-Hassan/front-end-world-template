fc6dfa0cfc1b53d6938136d22757b504fc6b7746
Mohamed Hassan <mohamed.h.ismael@gmail.com>
2024-11-27 23:43:41 +0200

    Add HTML, CSS, JS files.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let arr = [
+    "HTML is the foundation of all web pages. It's the standard markup language used to structure content on the web.",
+    "CSS (cascading style sheets) is used to style and layout web pages, making them visually appealing and responsive.",
+    "JavaScript adds interactivity to websites, making them dynamic and engaging for users."
+];
+function fun(i) {
+    alert(arr[i]);
+}
